911589bcb4a093784d6fe7239b890eb1a72ecde0
MLFAMG007 <76630003+MLFAMG007@users.noreply.github.com>
2021-03-08 15:05:14 +0600

    can be solved by O(1)

--- 1225 - Palindromic Numbers (II).cpp
+++ 1225 - Palindromic Numbers (II).cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main()
 {
     //ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    // you can also do it by O(1) 
+    // check this out 
+    // 
 
     ll t; cin >> t;
     ll r = 1;
